b7792f3b4125ed3b3bdd4db68380e5787938af67
Pragati Prasanna <pragatiprasanna@Pragatis-MBP.bbrouter>
2023-06-23 17:23:06 +0530

    change backend for render

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -12,7 +12,7 @@ export const useLogin = () => {
         setError(null);
 
         try {
-            const response = await fetch('https://diary-api-ba90.onrender.com/user/login', {
+            const response = await fetch('https://diary-api-ba90.onrender.com/api/user/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
